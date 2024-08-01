7f0562da85018cc381c31ed2ad26c770ebc31e7a
Daniil <hikiray665@gmail.com>
2024-08-01 20:55:58 +0300

    Initial Commit

---
+++ IOS/IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp.swift
+//  IOS
+//
+//  Created by Даниил Ледник on 01.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
